6420413825230a1d380ffe55329a896f1d1be658
nsms556 <nsms556@kakao.com>
2021-04-29 20:56:51 +0900

    add lv3-5(bingo)

--- Week1/Day5/lv3-6.py
+++ Week1/Day5/lv3-6.py
@@ -11,14 +11,14 @@ def solution(board, nums):
             bingo[board[i][j]] = (i, j)
 
     for num in nums :
-        i, j = bingo[num]
+        y, x = bingo[num]
 
-        hori[j] += 1
-        vert[i] += 1
+        hori[x] += 1
+        vert[y] += 1
 
-        if i == j :
+        if x == y :
             diag[0] += 1
-        elif j == n - i - 1 :
+        if y == n - x - 1 :
             diag[1] += 1
 
     print(hori, vert, diag)
@@ -26,3 +26,4 @@ def solution(board, nums):
 
 print(solution([[11,13,15,16],[12,1,4,3],[10,2,7,8],[5,14,6,9]], [14,3,2,4,13,1,16,11,5,15]))
 print(solution([[6,15,17,14,23],[5,12,16,13,25],[21,4,2,1,22],[10,20,3,18,8],[11,9,19,24,7]], [15,7,2,25,9,16,12,18,5,4,10,13,20]))
+print(solution([[1,2,3], [4,5,6], [7,8,9]], [1,5,9,7,3]))
